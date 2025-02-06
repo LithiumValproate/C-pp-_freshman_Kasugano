680c5a347431057c73b874e66e2e23991103a94a
Lithium Valproate <144132372+LithiumValproate@users.noreply.github.com>
2025-02-06 17:17:53 +0800

    Add files via upload

---
+++ Luogu.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    unsigned long m;
+    cin >> n >> m;
+    int r[m];
+    for (int i = 0; i < m; i++)
+        cin >> r[i];
+    
+}
